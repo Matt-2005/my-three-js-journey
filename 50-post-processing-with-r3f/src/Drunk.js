ac48531ed6c56bafdf0b2dacf7b429ec6dc1d713
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-04-04 11:13:00 +0200

    three

--- 50-post-processing-with-r3f/src/Drunk.js
+++
@@ -1,9 +0,0 @@
-import { forwardRef } from 'react'
-import DrunkEffect from './DrunkEffect.js'
-
-export default forwardRef(function Drunk(props, ref) {
-  console.log(props)
-  const effect = new DrunkEffect(props, ref)
-
-  return <primitive ref={ref} object={effect} />
-})
